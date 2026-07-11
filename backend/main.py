b3116b3f5021ca961943cc81c821c5eea563f039
Areeb Dastgeer <areebdastgeer.link@gmail.com>
2026-07-11 18:32:28 +0500

    Allow Vercel deployment URLs

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://ramp-up-five.vercel.app",
-    "https://ramp-g9gr08jm6-grad-ai.vercel.app","https://ramp-up-ai.vercel.app/"],
+    "https://ramp-g9gr08jm6-grad-ai.vercel.app","https://ramp-up-ai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
